a27049e10016a96adb31dba45f89bc9e89f914a1
Igor <26maius@gmail.com>
2021-05-24 20:42:51 +0300

    add: date into post, styles

--- public/js/main.js
+++ public/js/main.js
@@ -18,11 +18,19 @@ async function renderPosts() {
     let postOutput = '';
 
     posts.forEach(post => {
-        console.log(post.title)
+        let postDate = post.date.split(" ")[0].split("-")
+        let date = new Date(postDate[0], postDate[1], postDate[2])
+        let shortDay = date.toLocaleString('en-us', { weekday: 'short'});
+        let shortMonth = date.toLocaleString('en-us', { month: 'short' });
+        let shortYear = date.toLocaleString('en-us', { year: '2-digit' });
+
+        let fullDate = shortDay + ", " + shortMonth + " " + shortYear;
+
         let item = `<div class="post">
+                        <span class="post_date">${fullDate}</span>
                         <img src="${post.image}" alt="${post.title}" class="post_pic" />
                         <h2 class="post_title">${post.title}</h2>
-                        <div class="post_description">${post.description}</div>
+                        <p class="post_description">${post.description}</p>
                     </div>`
 
         postOutput += item
